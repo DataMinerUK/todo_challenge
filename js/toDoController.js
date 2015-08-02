471a3917ad0d7caa2bdeeb26cefa76d9fd3083d2
Nicola Hughes <nicolarhughes@gmail.com>
2015-08-02 20:07:33 +0100

    Letting user know how many tasks are left to do

--- js/toDoController.js
+++ js/toDoController.js
@@ -1,11 +1,34 @@
 toDo.controller('ToDoCtrl', [function() {
   var self = this;
   self.toDoList = [];
+  self.completed = [];
+  self.tasksRemaining = 0;
   self.addTask = function() {
     self.toDoList.push(self.task)
     self.task = ''
+    console.log(self.completed)
+    console.log(self.toDoList)
   }
   self.removeTask = function(index) {
+    var taskInQuestion = self.toDoList[index]
     self.toDoList.splice(index,1)
+    if (self.completed.indexOf(taskInQuestion) > -1) {
+      var index = self.completed.indexOf(taskInQuestion)
+      self.completed.splice(index,1)
+    }
+    console.log(self.completed)
+    console.log(self.toDoList)
+  }
+
+  self.completeTask = function(index) {
+    var taskCompleted = self.toDoList[index]
+    self.completed.push(taskCompleted)
+    console.log(self.completed)
+    console.log(self.toDoList)
+  }
+
+  self.tasksLeft = function() {
+    self.tasksRemaining = self.toDoList.length - self.completed.length
+    return self.tasksRemaining
   }
 }]);
